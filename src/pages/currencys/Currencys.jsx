64db49f86b54d3b96995c09015307520b57ed9b7
Pedro Gabriel <pedrogabriellima@gmail.com>
2024-03-06 21:53:18 -0300

    Style: Currencys Page Styling tablet/desktop

--- src/pages/currencys/Currencys.jsx
+++ src/pages/currencys/Currencys.jsx
@@ -11,32 +11,32 @@ export default function Currencys ({ baseCode, currencys, coins }) {
     // const filteredCoins = coinsFilter(coins, 10);
     // console.log(filteredCoins)
     return (
-        <main className="flex flex-col justify-center items-center pt-24">
-            <h1 className="text-black text-2xl font-semibold mb-4">Moedas</h1>
-            <p className="text-black font-normal">Moeda Base: {baseCode}</p>
-            <p className="text-xs text-center font-light p-2">Caso necessário acesse o menu para alterar a moeda base.</p>
+        <main className="flex flex-col justify-center items-center pt-16 md:pt-24">
+            <h1 className="text-black text-2xl font-semibold my-6 md:my-10 md:text-3xl">Moedas</h1>
+            <p className="text-base text-black font-normal md:text-lg">Moeda Base: {baseCode.toUpperCase()}</p>
+            <p className="text-xs text-center font-light px-4 py-2 md:text-base">Caso necessário acesse o menu para alterar a moeda base.</p>
             {
                 currencys ? 
-                <table className="table-auto w-full border-separate border-spacing-2">
+                <table className="table-auto w-full max-w-md border-separate border-spacing-2 md:max-w-lg">
                     <thead>
-                        <tr>
-                            <th className="bg-gray-700 text-white p-2">Codigo Iso</th>
-                            <th className="bg-gray-700 text-white p-2">Valor</th>
+                        <tr className="bg-black hover:bg-slate-950">
+                            <th className="text-base bg-gray-700 text-white p-3">Codigo ISO</th>
+                            <th className="text-base bg-gray-700 text-white p-3">Valor</th>
                         </tr>
                     </thead>
                     <tbody>
                     {coins.map((coin) => {
                         return (
                             <tr key={coin[0]}>
-                                <td className="bg-gray-500 text-white p-2 text-center">{coin[0]}</td>
-                                <td className="bg-gray-500 text-white p-2 text-center">{coin[1]}</td>
+                                <td className="text-base bg-gray-500 text-white p-2 text-center">{coin[0]}</td>
+                                <td className="text-base bg-gray-500 text-white p-2 text-center">{coin[1]}</td>
                             </tr>
                         )
                     })}
                     </tbody>
                 </table>
                  : 
-                <h2><Loading /></h2>
+                <Loading />
             }    
         </main>
     )
