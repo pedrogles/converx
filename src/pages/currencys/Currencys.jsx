73c53b8e90d57a4f45688df9449cac135c924efa
Pedro Gabriel <pedrogabriellima@gmail.com>
2024-03-23 11:32:37 -0300

    Feat: Filter currencys

--- src/pages/currencys/Currencys.jsx
+++ src/pages/currencys/Currencys.jsx
@@ -11,6 +11,8 @@ export default function Currencys () {
     const [newBaseCode, setNewBaseCode] = useState("");
     // Coins {key:value}
     const [coins, setCoins] = useState([]);
+    // Search Coin
+    const [searchCoin, setSearchCoin] = useState("");
     // Filter Index Coins
     const [filterIndexCoins, setFilterIndexCoins] = useState(8);
     // IsoCode
@@ -29,30 +31,39 @@ export default function Currencys () {
         setIsoCode(Object.keys(currencys.conversion_rates))
         }
     }, [currencys]);
-    const change_base_code = () => {
+    // Change base code
+    const handle_change_base_code = () => {
         if(isoCode.includes(newBaseCode.toUpperCase())) {
             setBaseCode(newBaseCode);
         } else ( 
             window.alert("Escolha uma moeda válida")
         );
     };
+    // Load more coins
     const handle_load_more = () => {
         const button = document.getElementById("load-more")
         setFilterIndexCoins(filterIndexCoins + 8);
         if(filterIndexCoins >= coins.length) {
-            button.innerText = "Chegamos ao fim"
+            button.innerText = "Fim da lista"
         }
     };
+    // Filter currency
+    const filteredCoins = coins.filter((coin) => {
+        const upperSearch = searchCoin.toUpperCase();
+        return coin[0].includes(upperSearch)
+    }, [searchCoin]);
+
     return (
         <main className="flex flex-col justify-center items-center pt-16 md:pt-24">
             <h1 className="text-black text-2xl font-semibold mt-6 md:mt-10 md:text-3xl">Moedas</h1>
-            <div className="flex flex-col justify-center items-center gap-2 my-8">
+            {/* Change Basecode */}
+            <div className="flex flex-col justify-center items-center gap-4 my-8">
                 <div className="flex justify-center items-center flex-wrap gap-4">
-                    <p className="text-lg text-black" >{baseCode.toUpperCase()}</p>
+                    <label htmlFor="basecode" className="text-lg text-black" >{baseCode.toUpperCase()}</label>
                     <input 
                         className="max-w-40 border-2 rounded-md text-md p-2 focus:outline-rose-600 md:max-w-52"
+                        id="basecode"
                         type="search" 
-                        name="search" 
                         placeholder="Moeda base" 
                         list="isos"
                         onChange={e => setNewBaseCode(e.target.value)}
@@ -67,11 +78,22 @@ export default function Currencys () {
                 </div>
                 <button 
                     className="text-white bg-blue-900 w-full text-sm py-2 px-6 rounded-md duration-500 hover:bg-blue-800"
-                    onClick={() => change_base_code(newBaseCode)}>
+                    onClick={() => handle_change_base_code(newBaseCode)}>
                     Alterar
                 </button>
             </div>
+            {/* Search */}
+            <div className="flex justify-center items-center flex-wrap gap-4 mb-6">
+                <label htmlFor="search-coin" className="text-lg text-black">Busca:</label>
+                <input 
+                    id="searc-coin"
+                    className="max-w-40 border-2 rounded-md text-md p-2 focus:outline-rose-600 md:max-w-52"
+                    type="search" 
+                    onChange={e => setSearchCoin(e.target.value)}
+                />
+            </div>
             <p className="text-xs text-center font-light px-4 py-2 md:text-base">Valores de acordo com a moeda base definida.</p>
+            {/* Conditiona Rendering: Coins/Loading */}
             {
                 currencys ? 
                 <>
@@ -83,7 +105,7 @@ export default function Currencys () {
                             </tr>
                         </thead>
                         <tbody>
-                        {coins.slice(0, filterIndexCoins).map((coin) => {
+                        {filteredCoins.slice(0, filterIndexCoins).map((coin) => {
                             return (
                                 <tr key={coin[0]}>
                                     <td className="text-base bg-gray-500 text-white p-2 text-center">{coin[0]}</td>
