2cb50918fb7475255b64ae436d65f8fba8db050b
Pedro Gabriel <pedrogabriellima@gmail.com>
2024-10-10 12:31:25 -0300

    Feat: add providers in main for global state management

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import './index.css';
+import React from 'react';
+import { App } from './App.jsx';
+import ReactDOM from 'react-dom/client';
+import { CurrencysProvider, ConversionProvider } from './contexts';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ConversionProvider>
+      <CurrencysProvider>
+        <App />
+      </CurrencysProvider>
+    </ConversionProvider>
   </React.StrictMode>,
-)
+);
