7c53b6b12fb70c1a723bf1bd6bd1e29c7b9aaade
Pedro Gabriel <pedrogabriellima@gmail.com>
2025-06-13 12:30:58 -0300

    refactor: improve Footer semantics, spacing and use dynamic year variable

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 export { Table } from './table/Table';
 export { Input } from './inputs/Input';
 export { Navbar } from './navbar/Navbar';
-export { Footer } from './footer/Footer';
+export { Footer } from './Footer/Footer';
 export { Button } from './Button/Button';
 export { Loading } from './loading/Loading';
 export { SelectInput } from './inputs/SelectInput';
